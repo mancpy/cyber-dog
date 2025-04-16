450a97effde8684b67b04d2db90e693bba7104e5
Pedro Esteves <pedro@ataraxia.dev>
2025-04-16 16:51:17 -0300

    feat: add ping command for testing

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('ping').setDescription('Respond with Pong and the Cyber Dog\'s latency'),
+	async execute(interaction) {
+		const sent = await interaction.reply({ content: 'Calculating ping...', fetchReply: true });
+		const pingTime = sent.createdTimestamp - interaction.createdTimestamp;
+		await interaction.editReply(`Pong! 🏓\nCyber Dog\'s latency: ${pingTime}ms'`);
+	}
+}
